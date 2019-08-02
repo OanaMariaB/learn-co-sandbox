6a32e38d86c599208497130e9f821798cdc6a013
Oana Berentan <oana.berentan@yahoo.com>
2019-08-02 22:10:49 +0000

    test

---
+++ coding_bootcamps/lib/coding_bootcamps.rb
@@ -0,0 +1,9 @@
+require_relative "./coding_bootcamps/version"
+require_relative "./coding_bootcamps/cli"
+
+require 'pry'
+
+module CodingBootcamps
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ coding_bootcamps/lib/coding_bootcamps/version.rb
@@ -0,0 +1,3 @@
+module CodingBootcamps
+  VERSION = "0.1.0"
+end
